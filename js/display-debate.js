b527e2e4110326fe626660d2896b63c4c4957200
Amos Ng <amosjyng@gmail.com>
2013-12-25 20:19:11 -0500

    Can now display multiple argument cards

--- js/display-debate.js
+++ js/display-debate.js
@@ -1,28 +1,37 @@
-var card_width = "300";
-var card_height = "186";
+var card_width = 300;
+var card_height = 186;
 
 var node0 = {
     "summary": "Why a max length of 140 characters? Well, it sure as hell works for Twitter. Seems like just enough to pack some good info into an argument."
+}
+
+var node1 = {
+    "summary": "Taxation is theft."
 }
 
 function draw_node(node, x, y) {
     $("svg#graph").append(build_node_rect(node));
 }
 
+function x_pos(node, i) {
+    return i * (card_width + 10) + 10;
+}
+
 function draw_graph() {
     var svg = d3.select("svg#graph");
-    var html_nodes = svg.selectAll("g").data([node0]);
+    var html_nodes = svg.selectAll("g").data([node0, node1]);
     var new_nodes = html_nodes.enter().append("g").classed("argument", true);
     new_nodes.append("rect").attr("width", card_width)
-        .attr("height", card_height).attr("x", "10").attr("y", "10");
+        .attr("height", card_height).attr("x", x_pos).attr("y", 10);
     var switch_objects = new_nodes.append("switch");
     switch_objects.append("foreignObject")
             .attr("requiredFeatures", "http://www.w3.org/TR/SVG11/feature#Extensibility")
             .attr("width", card_width).attr("height", card_height)
-            .attr("x", "10").attr("y", "10").append("xhtml:div")
+            .attr("x", x_pos).attr("y", "10").append("xhtml:div")
             .attr("class", "summary").append("p")
             .classed("summary", true).text(function (d) {
                 return d.summary;
             }).attr("xmlns", "http://www.w3.org/1999/xhtml");
-    switch_objects.append("text").attr("x", "10").attr("y", "10").text("Sorry, your  browser is not supported.")
+    switch_objects.append("text").attr("x", x_pos).attr("y", "10")
+        .text("Sorry, your browser is not supported.")
 }
