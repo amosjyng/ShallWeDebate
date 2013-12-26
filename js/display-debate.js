a8a482fda08b3439f971ab01cc885cc7fd2b2fda
Amos Ng <amosjyng@gmail.com>
2013-12-25 19:04:15 -0500

    Used SVG instead to display argument card

--- js/display-debate.js
+++ js/display-debate.js
@@ -1,19 +1,28 @@
-function resize_canvas() {
-    var div = document.getElementById("graph_container");
-    var canvas = document.getElementById("graph");
-    canvas.height = div.offsetHeight;
-    canvas.width  = div.offsetWidth;
+var card_width = "300";
+var card_height = "186";
+
+var node0 = {
+    "summary": "Why a max length of 140 characters? Well, it sure as hell works for Twitter. Seems like just enough to pack some good info into an argument."
+}
+
+function draw_node(node, x, y) {
+    $("svg#graph").append(build_node_rect(node));
 }
 
 function draw_graph() {
-	var canvas = document.getElementById('graph');
-	if (canvas.getContext) {
-        var ctx = canvas.getContext("2d");
-
-        ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect (10, 10, 55, 50);
-
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect (30, 30, 55, 50);
-    }
+    var svg = d3.select("svg#graph");
+    var html_nodes = svg.selectAll("g").data([node0]);
+    var new_nodes = html_nodes.enter().append("g").classed("argument", true);
+    new_nodes.append("rect").attr("width", card_width)
+        .attr("height", card_height).attr("x", "10").attr("y", "10");
+    var switch_objects = new_nodes.append("switch");
+    switch_objects.append("foreignObject")
+            .attr("requiredFeatures", "http://www.w3.org/TR/SVG11/feature#Extensibility")
+            .attr("width", card_width).attr("height", card_height)
+            .attr("x", "10").attr("y", "10").append("xhtml:div")
+            .attr("class", "summary").append("p")
+            .classed("summary", true).text(function (d) {
+                return d.summary;
+            }).attr("xmlns", "http://www.w3.org/1999/xhtml");
+    switch_objects.append("text").attr("x", "10").attr("y", "10").text("Sorry, your  browser is not supported.")
 }
