bbe09c5caa98920c09503bef74d37228b78663f7
Amos Ng <amosjyng@gmail.com>
2014-01-17 10:49:10 -0800

    Added postgresql recipe

--- chef/cookbooks/debate/recipes/default.rb
+++ chef/cookbooks/debate/recipes/default.rb
@@ -10,15 +10,5 @@
 package("default-jdk")
 
 bash "Set up and run Play server" do
-  user "vagrant"
-  code <<-EOH
-  cd /vagrant
-  play compile stage
-  start_script="/vagrant/target/universal/stage/bin/shallwedebate > debate-\$(date +%s).log &"
-  if ! grep -Fxq "$start_script" ~/.bashrc
-  then
-    echo "$start_script" >> ~/.bashrc
-  fi
-  exit 0
-  EOH
+  # nothing for now
 end
