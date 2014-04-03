3dc6463049fdc1f2d79ea20c109c9e821427be5a
Amos Ng <amosjyng@gmail.com>
2014-04-03 15:23:11 -0400

    Got it to return a TODO page for arguments.

---
+++ app/controllers/Argument.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Argument extends Controller
+{
+    public static Result viewTask(Long id)
+    {
+        return TODO;
+    }
+}
