c741aa50ada2dbe1ddf64a3f72ff4ec1ac315979
Amos Ng <amosjyng@gmail.com>
2015-05-11 10:37:24 -0400

    Removed now-unused files

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git pull
-sudo killall java
-sudo rm /home/ec2-user/ShallWeDebate/nohup.out
-play clean compile stage
-sudo -E nohup /home/ec2-user/ShallWeDebate/target/universal/stage/bin/shallwedebate -Dhttp.port=80 &
-sleep 5 # wait for nohup command to run
-sudo cat /home/ec2-user/ShallWeDebate/nohup.out
